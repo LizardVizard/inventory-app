a2a60d3a85da9f8a9b4a9ea4028d88fc9536eb9f
Maxim <195960242+LizardVizard@users.noreply.github.com>
2025-11-01 21:14:55 +0200

    chore(app): setup app
    - handle __dirname
    - set views engine and path
    - add routers

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+import genreRouter from "./routes/genreRouter.js";
+import gameRouter from "./routes/gameRouter.js";
+import developerRouter from "./routes/developerRouter.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,10 +30,12 @@ const app = express();
 //  - name
 // Many to many relationship:
 //  - Justion table, i.e. GameGenre(gameId, genreId)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.end("HI");
-});
+app.use("/genres", genreRouter);
+app.use("/games", gameRouter);
+app.use("/developers", developerRouter);
 
 app.listen(PORT, (req, res) => {
   console.log(`App is listening on PORT: ${PORT}`);
