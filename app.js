a544be759b4af18101abdf170f27aa0b66a5f3dc
Maxim <195960242+LizardVizard@users.noreply.github.com>
2025-11-02 20:42:38 +0200

    feat(view): add index page, change layout, add styling

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ import { dirname } from "path";
 import genreRouter from "./routes/genreRouter.js";
 import gameRouter from "./routes/gameRouter.js";
 import developerRouter from "./routes/developerRouter.js";
+import indexRouter from "./routes/indexRouter.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const assetsPath = path.join(__dirname, "public");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,23 +18,22 @@ const app = express();
 
 // TODO:
 // Games table
-//  - id
-//  - title
-//  - ? genres
-//  - studio
-//  - release date
-//  - price
-// Dev studio table
-//  - id
-//  - name
-// Genre table
-//  - id
-//  - name
-// Many to many relationship:
-//  - Justion table, i.e. GameGenre(gameId, genreId)
+//  - ? price
+// Other:
+//  - add view all to games and devs
+//  - CRUD actions
+//    - how to handle input fields
+//    - how to send data
+//  - index view has first 5 items,
+//    for games by release date, for developers by games developed,
+//    for genres count of games
+//  - delete functionality; how to handle items that depend on the item deleted
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(assetsPath));
+
+app.use("/", indexRouter);
 app.use("/genres", genreRouter);
 app.use("/games", gameRouter);
 app.use("/developers", developerRouter);
